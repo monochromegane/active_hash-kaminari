d7aeecbc63a123f468b2117a75be1de5f863fed6
monochromegane <dev.kuro.obi@gmail.com>
2016-02-29 00:28:29 +0900

    Supproted custom find all method.

--- lib/active_hash/paginatable.rb
+++ lib/active_hash/paginatable.rb
@@ -7,13 +7,28 @@ module ActiveHash
     end
 
     module ClassMethods
-      [:find, :all, :where].each do |method|
+      [:find, :all, :where, :method_missing].each do |method|
         define_method(method) do |*args|
           paginatize(super(*args))
         end
       end
 
       private
+
+      def define_custom_find_all_method(field_name)
+        super(field_name)
+
+        method_name         = :"find_all_by_#{field_name}"
+        orignal_method_name = :"#{method_name}_org"
+
+        the_meta_class.instance_eval do
+          alias_method orignal_method_name, method_name
+
+          define_method(method_name) do |*args|
+            paginatize(send(orignal_method_name, *args))
+          end
+        end
+      end
 
       def paginatize(ary)
         return ary unless ary.is_a?(Array)
