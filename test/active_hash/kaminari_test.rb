dffad22e41bde831609a30eb170305a617ac0894
monochromegane <dev.kuro.obi@gmail.com>
2016-02-29 01:12:40 +0900

    Added tests.

--- test/active_hash/kaminari_test.rb
+++ test/active_hash/kaminari_test.rb
@@ -5,7 +5,38 @@ class ActiveHash::KaminariTest < Minitest::Test
     refute_nil ::ActiveHash::Kaminari::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  class Country < ActiveHash::Base
+    prepend ActiveHash::Paginatable
+    self.data = [
+      {:id => 1, :name => "US"},
+      {:id => 2, :name => "Canada"}
+    ]
+  end
+
+  def test_find_with_array_returns_paginatable_array
+    assert_instance_of(::Kaminari::PaginatableArray, Country.find([1, 2]))
+  end
+
+  def test_all_returns_paginatable_array
+    assert_instance_of(::Kaminari::PaginatableArray, Country.all)
+  end
+
+  def test_where_returns_paginatable_array
+    assert_instance_of(::Kaminari::PaginatableArray, Country.where(name: 'US'))
+  end
+
+  def test_custom_find_all_returns_paginatable_array
+    # First, returns array from method missing
+    assert_instance_of(::Kaminari::PaginatableArray, Country.find_all_by_name('US'))
+    # Second, returns array from defined method
+    assert_instance_of(::Kaminari::PaginatableArray, Country.find_all_by_name('US'))
+  end
+
+  def test_paginatable_array_has_page_scope_methods
+    paginatable_array = Country.all
+
+    %i(per padding total_pages current_page next_page prev_page first_page? last_page? out_of_range?).each do |method|
+      assert_respond_to(paginatable_array, method)
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_hash/kaminari'
+require 'active_hash'
+
+# Avoid kaminari gem's warning 'no framework detected.'
+module Sinatra; end
+
+require 'kaminari'
+require 'kaminari/models/array_extension'
 
 require 'minitest/autorun'
